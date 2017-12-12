8d6e3ddedddd2ca995c55cf190e33b2d647d2f1b
Alexandre GC <a.gaspardcilia@gmail.com>
2017-12-12 09:02:06 +0100

    Database

---
+++ Xenon.Database/Users.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table1]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [username] NCHAR(32) NOT NULL, 
+    [password] NCHAR(64) NOT NULL, 
+    [type] INT NOT NULL, 
+    [mail] NCHAR(32) NULL
+)
---
+++ Xenon.Database/Wallets.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Wallets]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [service] NCHAR(32) NOT NULL
+)
